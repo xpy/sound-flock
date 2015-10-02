af832a1489dcc24be0073e0ed33ca82f2ac7e85e
xpy <pavloschris@gmail.com>
2015-10-02 19:04:41 +0300

    Partial tone distortion constructor

--- src/xpy/sound_flock/Distortions/PartialToneDistortion.java
+++ src/xpy/sound_flock/Distortions/PartialToneDistortion.java
@@ -19,6 +19,14 @@ public class PartialToneDistortion implements Distortion {
     private int    numOfDistortions;
     private Phrase phrase;
 
+    private static final int POSITION_RANDOM = 0;
+    private static final int POSITION_START  = 1;
+    private static final int POSITION_END    = 2;
+
+    private static final int PATTERN_RANDOM = 0;
+    private static final int PATTERN_UP     = 1;
+    private static final int PATTERN_DOWN   = 2;
+
     public PartialToneDistortion (Phrase phrase) {
 
         Random r = new Random();
@@ -35,6 +43,57 @@ public class PartialToneDistortion implements Distortion {
             tuneAmounts.add(r.nextInt(9) - 4);
         }
     }
+
+    public PartialToneDistortion (Phrase phrase, int position, int pattern, int num) {
+
+        Random r = new Random();
+        this.phrase = phrase;
+        numOfDistortions = Math.min(this.phrase.numOfNotes, num);
+        int start, end;
+        switch (position) {
+            default:
+                start = 0;
+                end = numOfDistortions;
+                int noteIndex;
+                for (int i = start; i < end; i++) {
+
+                    do {
+                        noteIndex = r.nextInt(this.phrase.numOfNotes);
+                    } while (noteIndexes.contains(noteIndex));
+                    noteIndexes.add(noteIndex);
+                }
+                break;
+            case (POSITION_START):
+                start = 0;
+                end = numOfDistortions;
+                for (int i = start; i < end; i++) noteIndexes.add(i);
+                break;
+            case (POSITION_END):
+                start = numOfDistortions;
+                end = this.phrase.numOfNotes;
+                for (int i = start; i < end; i++) noteIndexes.add(i);
+                break;
+        }
+        switch (pattern) {
+            default:
+                for (int i = start; i < end; i++) {
+                    tuneAmounts.add(r.nextInt(9) - 4);
+                }
+                break;
+            case (PATTERN_UP):
+                for (int i = start; i < end; i++) {
+                    tuneAmounts.add((r.nextInt(5) + 1) * -1);
+                }
+
+                break;
+            case (PATTERN_DOWN):
+                for (int i = start; i < end; i++) {
+                    tuneAmounts.add((r.nextInt(5) + 1) * -1);
+                }
+                break;
+        }
+    }
+
 
     @Override
     public void apply () {
